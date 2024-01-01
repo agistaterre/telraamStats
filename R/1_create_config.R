9fc01430d641b5684f211bd1b599e091e46729da
Ketsia Guichard <ketsia.guichard@gmail.com>
2024-01-01 16:23:13 +0100

    manage config creation in package

---
+++ R/1_create_config.R
@@ -0,0 +1,26 @@
+#'  Create config file if needed.
+#'
+#'  If the user need to specify the IDs of his own sensors,
+#'  this function create a local configuration template file
+#'  in the 'inst' directory, to edit with specific informations.
+#'
+#' @export
+#'
+#' @importFrom yaml
+#'
+#'
+create_config <- function(){
+  file_path = "inst/config.yml"
+  template <- list("default"=list("url"="https://telraam-api.net/v1"),
+                   "segments"=list("segment-01"="idsegment","segment-02"="idsegment"))
+  if(!file.exists(file_path)){
+    if(!dir.exists("inst/")){
+      dir.create("inst/")
+    }
+    file.create(file_path)
+    yaml::write_yaml(template, file_path)
+  }
+  else {
+    warning("A configuration file already exists in the 'inst' directory")
+  }
+}
