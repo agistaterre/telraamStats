b91b0ee392bda8099e62b0d7c81a0494a83217c3
Ketsia Guichard <ketsia.guichard@gmail.com>
2024-01-16 11:37:59 +0100

    modify import function to take name instead of id in argument)

--- R/2_write_update_data.R
+++ R/2_write_update_data.R
@@ -2,7 +2,7 @@
 #'
 #' Writes or updates the sensor data in the data folder. It retrieves the data for the specified sensor between \code{start_date} and \code{end_date} (inclusive) using the \code{retrieve_sensor} function, and then converts certain columns to character strings before writing the data to a RData file in the data folder.
 #'
-#' @param id_sensor Numeric. ID of the sensor
+#' @param segment_name Character. Name of the segment, as specified in config.
 #' @param start_date Date. Start date "aaaa-mm-jj"
 #' @param end_date Date. End date "aaaa-mm-jj"
 #'
@@ -10,17 +10,21 @@
 #'
 #' @export
 #'
-write_update_data <- function(id_sensor, start_date, end_date){
+write_update_data <- function(segment_name, start_date, end_date){
 
   # Preparation of the dataset
-  data <- retrieve_sensor(id_sensor,start_date, end_date)
+  data <- retrieve_sensor(segment_name,start_date, end_date)
+
+  # Handle the case where there is no data
+  if(nrow(data) == 0){
+    error("No data for this period")
+  }
+
   # conversion from a numeric vector to a character string of car_speed_hist_0to70plus and car_speed_hist_0to120plus
   data$car_speed_hist_0to70plus <- sapply(data$car_speed_hist_0to70plus, function(x) paste(x, collapse = ", "))
   data$car_speed_hist_0to120plus <- sapply(data$car_speed_hist_0to120plus, function(x) paste(x, collapse = ", "))
 
-  telraam_segments <- get_segments()
-  name_segment <- names(telraam_segments[telraam_segments==id_sensor])
-  file_name <- paste0("data/",name_segment,".RData")
+  file_name <- paste0("data/",segment_name,".RData")
 
   if (!is.null(data)){
     if (file.exists(file_name)){
