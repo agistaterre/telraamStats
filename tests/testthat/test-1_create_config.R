fdaf5035e18f299b433e161945dfcd5b28512dd6
Ketsia Guichard <ketsia.guichard@gmail.com>
2024-01-19 15:11:24 +0100

    add warning tests

--- tests/testthat/test-1_create_config.R
+++ tests/testthat/test-1_create_config.R
@@ -1,11 +1,15 @@
 test_that("create configuration folder if needed", {
-  create_config()
+  if(dir.exists("inst/")){
+    expect_warning(create_config())
+  } else {
+    expect_no_warning(create_config())
+  }
   expect_true(dir.exists("inst/"))
   expect_true(file.exists("inst/config.yml"))
 })
 
 test_that("check yml arguments", {
-  create_config()
+  expect_warning(create_config())
   configuration <- config::get(file = "inst/config.yml")
   expect_identical(sort(names(configuration)), c('segments','url'))
 })
