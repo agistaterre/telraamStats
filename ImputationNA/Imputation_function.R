a042c015b472e95a6c335879e695c6ff1a5a3243
« Slazz26 » <paul.vallee1@icloud.com>
2024-05-31 12:55:22 +0200

    Imputation NA par discrimination des heures et jours

--- ImputationNA/Imputation_function.R
+++ ImputationNA/Imputation_function.R
@@ -2,37 +2,37 @@
 TimeSeries_Imputation <- function(data) {
   # Load the prophet library
   library(prophet)
-  
+
   # Prepare the training set by creating 'ds' and 'y' columns
   train_set <- data %>% mutate(ds = date, y = ifelse(uptime_quality, car + heavy, NA)) %>% select(ds, y)
   # Prepare the test set by filtering out rows where uptime_quality is false
   test_set <- data %>% filter(!uptime_quality) %>% mutate(ds = date) %>% select(ds)
-  
+
   # Prepare the holidays data for the Prophet model
-  holidays <- data %>% filter(holiday) %>% mutate(lower_window = 0, upper_window = 1, vacation = as.character(vacation)) %>% 
+  holidays <- data %>% filter(holiday) %>% mutate(lower_window = 0, upper_window = 1, vacation = as.character(vacation)) %>%
     select(vacation, date, lower_window, upper_window) %>% rename(holiday = vacation, ds = date)
-  
+
   # Train the Prophet model using the training set and holidays
   model <- prophet(train_set, holidays = holidays)
   # Predict values using the test set
   pred <- predict(model, test_set)
-  
+
   # Adjust predictions on the training set to obtain the fitting curve
   ajust <- predict(model, train_set)
   fitting_curve <- plot(model, ajust)
-  
+
   # Plot the components of the model
   components <- prophet_plot_components(model, pred)
-  
+
   # Prepare the prediction data for plotting
   dpred <- pred %>% select(ds, yhat) %>% rename(y = yhat) %>% mutate(type = "pred", y = ifelse(y < 0, 0, y))
   # Prepare the real data for plotting
   dtrain <- train_set %>% na.omit() %>% select(ds, y) %>% mutate(type = "real")
-  
+
   # Convert 'ds' columns to POSIXct format for both data frames
   dtrain$ds <- as.POSIXct(dtrain$ds)
   dpred$ds <- as.POSIXct(dpred$ds)
-  
+
   # Create a ggplot of the input data, including both real and predicted values
   plot_inputdata <- ggplot() +
     geom_point(data = dtrain, aes(x = ds, y = y, color = type)) +
@@ -41,7 +41,7 @@ TimeSeries_Imputation <- function(data) {
     xlab("Date") +
     ylab("Nombre de véhicules") +
     theme(legend.position = "none")
-  
+
   # Return a list containing the prediction, fitting curve, components, and plot
   return(list(prediction = dpred, fitting_curve = fitting_curve, components = components, Imputation = plot_inputdata))
 }
@@ -51,41 +51,41 @@ TimeSeries_Imputation_list <- function(list_data) {
   # Load the prophet library
   library(prophet)
   list_pred <- list()
-  
+
   # Loop through each dataset in the list
   for (i in 1:length(list_data)) {
     data <- list_data[[i]]$data
-    
+
     # Prepare the training set by creating 'ds' and 'y' columns
     train_set <- data %>% mutate(ds = date, y = ifelse(uptime_quality, car + heavy, NA)) %>% select(ds, y)
     # Prepare the test set by filtering out rows where uptime_quality is false
     test_set <- data %>% filter(!uptime_quality) %>% mutate(ds = date) %>% select(ds)
-    
+
     # Prepare the holidays data for the Prophet model
-    holidays <- data %>% filter(holiday) %>% mutate(lower_window = 0, upper_window = 1, vacation = as.character(vacation)) %>% 
+    holidays <- data %>% filter(holiday) %>% mutate(lower_window = 0, upper_window = 1, vacation = as.character(vacation)) %>%
       select(vacation, date, lower_window, upper_window) %>% rename(holiday = vacation, ds = date)
-    
+
     # Train the Prophet model using the training set and holidays
     model <- prophet(train_set, holidays = holidays)
     # Predict values using the test set
     pred <- predict(model, test_set)
-    
+
     # Adjust predictions on the training set to obtain the fitting curve
     ajust <- predict(model, train_set)
     fitting_curve <- plot(model, ajust)
-    
+
     # Plot the components of the model
     components <- prophet_plot_components(model, pred)
-    
+
     # Prepare the prediction data for plotting
     dpred <- pred %>% select(ds, yhat) %>% rename(y = yhat) %>% mutate(type = "pred", y = ifelse(y < 0, 0, y))
     # Prepare the real data for plotting
     dtrain <- train_set %>% na.omit() %>% select(ds, y) %>% mutate(type = "real")
-    
+
     # Convert 'ds' columns to POSIXct format for both data frames
     dtrain$ds <- as.POSIXct(dtrain$ds)
     dpred$ds <- as.POSIXct(dpred$ds)
-    
+
     # Create a ggplot of the input data, including both real and predicted values
     plot_inputdata <- ggplot() +
       geom_point(data = dtrain, aes(x = ds, y = y, color = type)) +
@@ -94,12 +94,12 @@ TimeSeries_Imputation_list <- function(list_data) {
       xlab("Date") +
       ylab("Nombre de véhicules") +
       theme(legend.position = "none")
-    
+
     # Create a list containing the prediction, fitting curve, components, plot, and model
     output <- list(prediction = dpred, fitting_curve = fitting_curve, components = components, Imputation = plot_inputdata, model = model)
     list_pred[[i]] <- output
   }
-  
+
   # Return the list of predictions and plots
   return(list_pred)
 }
---
+++ ImputationNA/tetsProphet.R
@@ -0,0 +1,94 @@
+# Charger les bibliothèques nécessaires
+library(ggplot2)
+library(prophet)
+library(dplyr)
+
+# Générer la séquence de dates
+ds = seq(as.Date("2017-01-01"), as.Date("2019-09-27"), by = "days")
+
+# Définir la fonction y. Vous pouvez changer cette fonction selon vos besoins.
+x = seq(1, length(ds), 1)
+y = x * sin(x^2)
+
+
+# Créer le data frame initial avec les dates et les valeurs de y
+data = data.frame(ds = ds, y = y)
+
+# Tracer les données initiales
+plot(data$ds, data$y, type = "l", main = "Données initiales")
+
+# Introduire des trous aléatoires dans les données (entre 1 et 30 valeurs consécutives manquantes)
+set.seed(123) # Pour reproductibilité
+for (i in 1:10) {
+  start = sample(1:(nrow(data) - 30), 1)
+  end = start + sample(1:50, 1)
+  data$y[start:end] = NA
+}
+
+# Récupérer les dates des valeurs supprimées
+data_na = data[is.na(data$y),]
+
+# Tracer les données avec valeurs manquantes
+plot(data$ds, data$y, type = "l", main = "Données avec valeurs manquantes")
+
+# Construire et ajuster le modèle Prophet
+model <- prophet(data.frame(ds = data$ds, y = data$y),seasonality.prior.scale=100,n.changepoints = 25)
+
+# Prédire les valeurs sur les dates initiales
+ajust <- predict(model, data.frame(ds = data$ds))
+fitting_curve <- plot(model, ajust)
+print(fitting_curve)
+
+# Prédire les valeurs manquantes
+pred <- predict(model, data_na)
+
+# Préparer les données pour l'affichage
+dpred <- pred %>%
+  select(ds, yhat) %>%
+  mutate(type = "pred") %>%
+  rename(y = yhat)
+
+dtrain <- data %>%
+  na.omit() %>%
+  select(ds, y) %>%
+  mutate(type = "real")
+
+# Convertir les colonnes ds en format POSIXct
+dtrain$ds <- as.POSIXct(dtrain$ds)
+dpred$ds <- as.POSIXct(dpred$ds)
+
+# Combiner les données réelles et prédites
+data_pred <- rbind(dtrain, dpred) %>%
+  arrange(ds)
+
+# Afficher l'imputation en traçant les données réelles et les données imputées
+ggplot(data_pred, aes(x = ds, y = y)) +
+  geom_line() +
+  geom_point(aes(color = type)) +
+  ggtitle("Imputation de données manquantes") +
+  xlab("Date") +
+  ylab("Valeurs") +
+  theme(legend.position = "none")
+
+#add_changepoints_to_plot(fitting_curve, model, threshold = 0.5)
+
+#Cross validation
+cv <- cross_validation(model, initial = 730, period =  180, horizon = 90  ,units = "days")
+print(cv)
+
+#Performance metrics
+performance_metrics(cv)
+
+plot_cross_validation_metric(cv, metric = "mape")
+plot_cross_validation_metric(cv, metric = "rmse")
+plot_cross_validation_metric(cv, metric = "mae")
+plot_cross_validation_metric(cv, metric = "coverage")
+
+
+
+
+
+
+
+
+
